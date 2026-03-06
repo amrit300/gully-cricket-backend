5cbb07a61665d7eec21a87202a8631f9faedbb82
amrit300 <126704837+amrit300@users.noreply.github.com>
2026-03-06 21:50:01 +0530

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -259,12 +259,12 @@ func getPlayers(c *fiber.Ctx) error {
 
 	if err != nil {
 
-		log.Println("DB QUERY ERROR:",err)
+	log.Println("DB QUERY ERROR:", err)
 
-		return c.Status(500).JSON(fiber.Map{
-			"error": err.Error(),
-		})
-	}
+	return c.Status(500).JSON(fiber.Map{
+		"error": err.Error(),
+	})
+}
 
 	defer rows.Close()
 
