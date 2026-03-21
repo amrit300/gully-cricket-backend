39525596faa59588b18c3050cb968b9ce928a4a5
amrit300 <126704837+amrit300@users.noreply.github.com>
2026-03-21 17:55:17 +0530

    Update match_sync.go

--- internal/match_sync.go
+++ internal/match_sync.go
@@ -1,41 +1,10 @@
-package internal
+package ingestion
 
 import (
 	"database/sql"
-	"log"
+	"gully-cricket/internal"
 )
 
-/*
-SyncMatches:
-Fetch matches from APIs → store in DB
-*/
-
-func SyncMatches(db *sql.DB) error {
-
-	matches, err := GetMatches(db)
-	if err != nil {
-		return err
-	}
-
-	for _, m := range matches {
-
-		_, err := db.Exec(`
-		INSERT INTO matches_master 
-		(team_a, team_b, venue, start_time, status)
-		VALUES ($1,$2,$3,$4,$5)
-		ON CONFLICT DO NOTHING
-		`,
-			m.TeamA,
-			m.TeamB,
-			m.Venue,
-			m.StartTime,
-			m.Status,
-		)
-
-		if err != nil {
-			log.Println("match insert error:", err)
-		}
-	}
-
-	return nil
+func SyncMatchesToDB(db *sql.DB) error {
+	return internal.SyncMatches(db)
 }
