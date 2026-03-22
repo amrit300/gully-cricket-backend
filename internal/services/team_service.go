43e27c70c18737c82769c538a9fa252564e12189
amrit300 <126704837+amrit300@users.noreply.github.com>
2026-03-22 23:14:44 +0530

    Update team_service.go

--- internal/services/team_service.go
+++ internal/services/team_service.go
@@ -2,24 +2,36 @@ package services
 
 import (
 	"database/sql"
+	"fmt"
 
 	"gully-cricket/internal/validators"
 )
 
 func CreateTeam(db *sql.DB, userID int, matchID int, playerIDs []int, captainID int, viceCaptainID int) error {
 
-	// Validate team
+	// 1. VALIDATIONS
+	if err := validators.ValidateMatchStatus(db, matchID); err != nil {
+		return err
+	}
+
+	if err := validators.ValidateTeamLimit(db, userID, matchID); err != nil {
+		return err
+	}
+
 	if err := validators.ValidateTeam(db, playerIDs, captainID, viceCaptainID); err != nil {
 		return err
 	}
 
+	// 2. TRANSACTION
 	tx, err := db.Begin()
 	if err != nil {
-		return err
+		return fmt.Errorf("db error")
 	}
+	defer tx.Rollback()
 
 	var teamID int
 
+	// 3. INSERT TEAM
 	err = tx.QueryRow(`
 		INSERT INTO teams (user_id, match_id, captain_player_id, vice_captain_player_id)
 		VALUES ($1,$2,$3,$4)
@@ -27,21 +39,25 @@ func CreateTeam(db *sql.DB, userID int, matchID int, playerIDs []int, captainID 
 	`, userID, matchID, captainID, viceCaptainID).Scan(&teamID)
 
 	if err != nil {
-		tx.Rollback()
 		return err
 	}
 
-	for _, p := range players {
-		_, err := tx.Exec(`
+	// 4. INSERT TEAM PLAYERS
+	for _, pid := range playerIDs {
+		_, err = tx.Exec(`
 			INSERT INTO team_players (team_id, player_id)
 			VALUES ($1,$2)
-		`, teamID, p)
+		`, teamID, pid)
 
 		if err != nil {
-			tx.Rollback()
 			return err
 		}
 	}
 
-	return tx.Commit()
+	// 5. COMMIT
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("commit failed")
+	}
+
+	return nil
 }
