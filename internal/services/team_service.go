a77978ee258eb8131ac006404748b9540413fc50
amrit300 <126704837+amrit300@users.noreply.github.com>
2026-03-22 16:13:40 +0530

    Create team_service.go

---
+++ internal/services/team_service.go
@@ -0,0 +1,48 @@
+package services
+
+import (
+	"database/sql"
+	"errors"
+
+	"gully-cricket/internal/validators"
+)
+
+func CreateTeam(db *sql.DB, userID int, matchID int, players []validators.Player, captainID int, viceCaptainID int) error {
+
+	// Validate team
+	if err := validators.ValidateTeam(players, captainID, viceCaptainID); err != nil {
+		return err
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	var teamID int
+
+	err = tx.QueryRow(`
+		INSERT INTO teams (user_id, match_id, captain_player_id, vice_captain_player_id)
+		VALUES ($1,$2,$3,$4)
+		RETURNING id
+	`, userID, matchID, captainID, viceCaptainID).Scan(&teamID)
+
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	for _, p := range players {
+		_, err := tx.Exec(`
+			INSERT INTO team_players (team_id, player_id)
+			VALUES ($1,$2)
+		`, teamID, p.ID)
+
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	return tx.Commit()
+}
