bde4eccc85d5bc3722f70834c144a56a82214601
amrit300 <126704837+amrit300@users.noreply.github.com>
2026-03-25 00:13:51 +0530

    Update wallet_service.go

--- internal/services/wallet_service.go
+++ internal/services/wallet_service.go
@@ -1,50 +1,7 @@
 package services
 
-import (
-	"database/sql"
-	"fmt"
-)
+import "database/sql"
 
-func DeductBalance(tx *sql.Tx, userID int, amount float64) error {
-
-	var balance float64
-
-	err := tx.QueryRow(`
-		SELECT wallet_balance FROM users WHERE id=$1 FOR UPDATE
-	`, userID).Scan(&balance)
-
-	if err != nil {
-		return err
-	}
-
-	if balance < amount {
-		return fmt.Errorf("insufficient balance")
-	}
-
-	_, err = tx.Exec(`
-		UPDATE users SET wallet_balance = wallet_balance - $1 WHERE id=$2
-	`, amount, userID)
-
-	return err
-}
-
-func CreditBalance(tx *sql.Tx, userID int, amount float64, reference string) error {
-
-	_, err := tx.Exec(`
-		UPDATE users SET wallet_balance = wallet_balance + $1 WHERE id=$2
-	`, amount, userID)
-
-	if err != nil {
-		return err
-	}
-
-	_, err = tx.Exec(`
-		INSERT INTO transactions (user_id, amount, transaction_type, reference_id)
-		VALUES ($1,$2,'credit',$3)
-	`, userID, amount, reference)
-
-	return err
-}
 func GetBalance(db *sql.DB, userID int) (float64, error) {
 
 	var balance float64
